93aa4aa24a0c91a12a967cdd52690257a089a55a
e1b19049 <e1b19049@st.oit.ac.jp>
2021-09-28 15:00:56 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1204/first/janken/JankenApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z1204.first.janken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1204/first/janken/JankenApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1204.first.janken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
