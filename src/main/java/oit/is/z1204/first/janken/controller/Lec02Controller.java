18fa1ccaa5c017515ca7357f6c78a0055b2f7b29
e1b19049 <e1b19049@st.oit.ac.jp>
2021-10-11 17:07:54 +0900

    ユーザ名入力機能追加

--- src/main/java/oit/is/z1204/first/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z1204/first/janken/controller/Lec02Controller.java
@@ -1,13 +1,22 @@
 package oit.is.z1204.first.janken.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+
 public class Lec02Controller {
+  @PostMapping("/lec02")
+  public String lec02(@RequestParam String name, ModelMap model) {
+    model.addAttribute("name", name);
+    return "lec02.html";
+  }
 
   @GetMapping("/lec02")
-  public String lec02() {
+  public String lec02Direct() {
     return "lec02.html";
   }
 }
