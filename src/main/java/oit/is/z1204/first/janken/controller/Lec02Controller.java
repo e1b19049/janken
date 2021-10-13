dc83f1ca095ba4c81ab22085c47bf6d934bcc1b0
e1b19049 <e1b19049@st.oit.ac.jp>
2021-10-13 10:40:36 +0900

    ログイン・ログアウト機能実装

--- src/main/java/oit/is/z1204/first/janken/controller/Lec02Controller.java
+++ src/main/java/oit/is/z1204/first/janken/controller/Lec02Controller.java
@@ -4,9 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is.z1204.first.janken.model.Janken;
 
@@ -14,14 +12,8 @@ import oit.is.z1204.first.janken.model.Janken;
 @RequestMapping("/lec02")
 
 public class Lec02Controller {
-  @PostMapping
-  public String lec02(@RequestParam String name, ModelMap model) {
-    model.addAttribute("name", name);
-    return "lec02.html";
-  }
-
   @GetMapping
-  public String lec02Direct() {
+  public String lec02() {
     return "lec02.html";
   }
 
---
+++ src/main/java/oit/is/z1204/first/janken/security/Lec03AuthConfiguration.java
@@ -0,0 +1,46 @@
+package oit.is.z1204.first.janken.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class Lec03AuthConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    // pass = admin
+    auth.inMemoryAuthentication().withUser("admin")
+        .password("$2y$10$y22POi3i0LN1DTDZ/XdFpeiY0cauPuydEeTgsRQtm7XrKaXwdIHfW").roles("ADMIN");
+
+    // pass = user
+    auth.inMemoryAuthentication().withUser("user")
+        .password("$2y$10$qFJxTZjJMFWF5o7gzBlZ1ukYwdMx3QICbbGMFDpD2WQ53y7u33Hsa").roles("USER");
+
+    // pass = test
+    auth.inMemoryAuthentication().withUser("test")
+        .password("$2y$10$lXgNIN6RlIOxF.PJeAIFDOiqP.uWCRK3EfdNQRe9M2qMzFLWywEK6").roles("USER");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // Spring Securityのフォームを利用してログインを行う
+    http.formLogin();
+
+    http.authorizeRequests().antMatchers("/lec02/**").authenticated();
+
+    // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8080/ に戻る
+    http.logout().logoutSuccessUrl("/");
+  }
+}
